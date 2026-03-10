d7cbd9fa7a5523f33209a6aa5e2fc7918d2a924b
Richard Holzner <richard.holzner@gmail.com>
2026-03-10 16:35:47 +0100

    Add reactive obj to lookup store

--- stores/lookup.store.js
+++ stores/lookup.store.js
@@ -1,23 +1,59 @@
 import { defineStore } from 'pinia';
-import { getManufacturers } from '~/services/bike.service';
-import { getModels } from '~/services/bike.service';
+import { getManufacturers, getModels, getSfClasses } from '~/services/lookup.service';
 
 export const useLookupStore = defineStore('lookup', () => {
-  const manufacturers = ref([]);
-  const models = ref([]);
+  const manufacturers = reactive({
+    bike: [],
+    car: [],
+  });
+
+  const models = reactive({
+    bike: [],
+    car: [],
+  });
+
+  const sfClasses = reactive({
+    car: [],
+  });
+  const loadingSfClasses = ref(false);
+
+  const loadingManufacturers = reactive({
+    bike: false,
+    car: false,
+  });
+
+  const loadingModels = reactive({
+    bike: false,
+    car: false,
+  });
 
   async function fetchManufacturers(type) {
-    manufacturers.value = await getManufacturers(type);
+    loadingManufacturers[type] = true;
+    manufacturers[type] = await getManufacturers(type);
+    loadingManufacturers[type] = false;
   }
 
-  async function fetchModels(type, id) {
-    models.value = await getModels(type, id);
+  async function fetchModels(type, manufacturerId) {
+    loadingModels[type] = true;
+    models[type] = await getModels(type, manufacturerId);
+    loadingModels[type] = false;
+  }
+
+  async function fetchSfClasses() {
+    loadingSfClasses.value = true;
+    sfClasses.car = await getSfClasses();
+    loadingSfClasses.value = false;
   }
 
   return {
     manufacturers,
     models,
+    sfClasses,
+    loadingManufacturers,
+    loadingModels,
+    loadingSfClasses,
     fetchManufacturers,
     fetchModels,
+    fetchSfClasses,
   };
 });
