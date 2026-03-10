93789305f5233edd554349007f74068a524195ac
Richard Holzner <richard.holzner@gmail.com>
2026-03-10 17:19:07 +0100

    Add api for annual-mileage

---
+++ server/api/car/annual-mileage.js
@@ -0,0 +1,6 @@
+// import sfClasses from '../../data/car/sf-classes.json';
+import annualMileage from '~/server/data/car/annual-mileage.json';
+
+export default defineEventHandler(() => {
+  return annualMileage;
+});
