cd58a8c66112628f79a8a85699ff77e97e74b726
Richard Holzner <richard.holzner@gmail.com>
2026-03-10 15:44:54 +0100

    Add api for car sfClasses

---
+++ server/api/car/sf-classes.get.js
@@ -0,0 +1,6 @@
+// import sfClasses from '../../data/car/sf-classes.json';
+import sfClasses from '~/server/data/car/sf-classes.json';
+
+export default defineEventHandler(() => {
+  return sfClasses;
+});
